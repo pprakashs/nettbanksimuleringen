22d7a32a65d6ccb81af3595577748992e88f675d
Prakash Shestha <prakash@Prakashs-MacBook-Pro.local>
2021-03-23 19:24:07 +0545

    step2 page created

--- pages/step2.js
+++ pages/step2.js
@@ -1,4 +1,4 @@
-const Home = () => {
+const Step2 = () => {
 	return (
 		<>
 			<header className="bg-white py-4 fixed w-full top-0 left-0">
@@ -161,21 +161,27 @@ const Home = () => {
 					</svg>
 				</div>
 			</header>
+
 			<section className="py-20">
 				<div className="container">
-					<div className="py-6 max-w-3xl ml-auto mr-auto">
-						<h1 className="text-white mb-6 text-[2rem]">Mine transaksjoner</h1>
-						<p className="text-white pb-8 text-xl">Innlogging i simulering av nettbank</p>
-						<p className="text-white pb-8 text-xl">
-							Velkommen til vår nettbanksimulering. Dette er en simulering, hvor ingen informasjon blir lagret. Du kan derfor trygt bruke ekte eller
-							fiktive tall når du blir bedt om å skrive inn i feltene.{' '}
+					<div className="my-6 max-w-3xl ml-auto mr-auto bg-white px-10 py-10 shadow-md">
+						<p className="text-black pb-8 text-xl">
+							Det finnes flere innloggingsmuligheter, blant annet med kodekort, SMS-koder og QR-koder. De fleste bankene har derimot BankID på mobil
+							eller innlogging med kodebrikker eller kodekort.
 						</p>
-						<p className="text-white pb-10 text-xl">
-							Det er litt forskjeller på utseende og bruk av begreper i nettbankene, men mye er likt. Sjekk din egen nettbank.
-						</p>
-						<a href="#" className="bg-white font-anenirHeavy rounded-md px-3 py-2 inline-block uppercase shadow-md font-medium text-text-xl">
-							start simulering
-						</a>
+						<h1 className="text-black mb-3">Logg inn i nettbanksimuleringen</h1>
+						<p className="text-black pb-12 text-xl">Ved å velge en av påloggingsmetodene under. </p>
+
+						<div className="flex space-x-8">
+							<a href="#" className="bg-primary p-8 text-white text-2xl">
+								<strong className="block font-anenirHeavy">BankID</strong>
+								Med kodebrikke
+							</a>
+							<a href="#" className="bg-primary p-8 text-white text-2xl">
+								<strong className="block font-anenirHeavy">BankID</strong>
+								På mobiltelefon
+							</a>
+						</div>
 					</div>
 				</div>
 			</section>
@@ -183,4 +189,4 @@ const Home = () => {
 	);
 };
 
-export default Home;
+export default Step2;
